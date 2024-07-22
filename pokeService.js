56b1cc95d2e780d0cd86bd516109c287f7a96da1
Darryl Kilzer <darrylkilzer@gmail.com>
2024-07-22 15:25:44 -0600

    front end request

--- pokeService.js
+++ pokeService.js
@@ -16,4 +16,25 @@ class PokemonService {
     const response = await this.axios.get(`${this.baseUrl}/${id}`);
     currentPokemon = response.data;
   }
+
+  //method to add a pokemon
+  async add(pokemon) {
+    const response = await this.axios.post(this.baseUrl, pokemon);
+    return response.data;
+  }
+
+  //method to update a pokemon
+  async update(pokemon) {
+    const response = await this.axios.put(
+      `${this.baseUrl}/${pokemon.id}`,
+      pokemon
+    );
+    return response.data;
+  }
+
+  //method to delete a pokemon
+  async delete(id) {
+    const response = await this.axios.delete(`${this.baseUrl}/${id}`);
+    return response.data;
+  }
 }
